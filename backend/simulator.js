65f462a66579726d99c5f9a1fdb33de517f29998
saiVamsi85 <akulasaivamsi@gmail.com>
2026-04-08 12:23:18 +0530

    modified simulator

--- backend/simulator.js
+++ backend/simulator.js
@@ -5,8 +5,8 @@ setInterval(async () => {
     temp: (20 + Math.random() * 12).toFixed(2),
     hum: (30 + Math.random() * 50).toFixed(2),
     light: Math.floor(Math.random() * 4095),
-    // motion: Math.random() > 0.5 ? 1 : 0,
-    motion: 1,
+    motion: Math.random() > 0.5 ? 1 : 0,
+    // motion: 1,
   };
 
   try {
