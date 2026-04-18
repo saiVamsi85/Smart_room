5ea27598e82aa42d203c50730dc601aed89f9a14
saiVamsi85 <akulasaivamsi@gmail.com>
2026-04-19 00:40:38 +0530

    add data files

--- backend/server.js
+++ backend/server.js
@@ -195,6 +195,5 @@ app.get("/latest", (req, res) => {
 });
 
 // start server
-app.listen(3000, () => {
-  console.log("Server running on http://localhost:3000");
-});
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on ${PORT}`));
